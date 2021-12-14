21cfa01c585d3dd94698c9b43cdeaae891c6b5f5
noion <m.davydov@tinkoff.ru>
2021-12-14 22:27:36 +0300

    print header data

--- src/main/java/ru/noion/jgbemu/Cartridge.java
+++ src/main/java/ru/noion/jgbemu/Cartridge.java
@@ -1,9 +1,12 @@
 package ru.noion.jgbemu;
 
 import ru.noion.jgbemu.header.CartridgeHeader;
+import ru.noion.jgbemu.header.NewLicenseeCode;
+import ru.noion.jgbemu.header.OldLicenseeCode;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class Cartridge {
@@ -15,11 +18,26 @@ public class Cartridge {
             System.out.println("Cartridge Loading:");
             var cartridgeHeader = new CartridgeHeader(romData);
             System.out.println(cartridgeHeader);
+            System.out.printf("Title %s%n", cartridgeHeader.getTitle());
+            printLicenseCode(cartridgeHeader);
+            var cartridgeType = ByteToHex.bytesToHex(cartridgeHeader.getCartridgeType());
             return true;
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Failed to load rom");
             return false;
         }
+    }
+
+    private void printLicenseCode(CartridgeHeader cartridgeHeader) {
+        if (cartridgeHeader.isSgb()) {
+            var licenseCode = new String(cartridgeHeader.getNewLicenseeCode(), StandardCharsets.US_ASCII);
+            var newLicenseeCode = NewLicenseeCode.getByCode(licenseCode);
+            System.out.printf("NewLicenseeCode %s%n", newLicenseeCode);
+            return;
+        }
+        var licenseCode = ByteToHex.bytesToHex(cartridgeHeader.getOldLicenseeCode());
+        var oldLicenseeCode = OldLicenseeCode.getByHexCode(licenseCode);
+        System.out.printf("OldLicenseeCode %s%n", oldLicenseeCode);
     }
 }
