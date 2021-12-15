5805f51d364bcc7278f1564e076840633a665fa6
noion <m.davydov@tinkoff.ru>
2021-12-15 23:19:01 +0300

    address mode enum

---
+++ src/main/java/ru/noion/jgbemu/cpu/AddressMode.java
@@ -0,0 +1,19 @@
+package ru.noion.jgbemu.cpu;
+
+public enum AddressMode {
+    REG_D16,
+    REG_D8,
+    REG_REG,
+    REG_MEM,
+    RGG_A8,
+    R_A16,
+    REG,
+    D16,
+    D8,
+    A16,
+    D16_REG,
+    D8_REG,
+    MEM_REG,
+    A8_REG,
+    A16_REG,
+}
