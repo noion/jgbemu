cdabf25de3d22c788a8eb67a1933b94ea1189770
noion <m.davydov@tinkoff.ru>
2021-12-15 23:05:21 +0300

    register type enum

---
+++ src/main/java/ru/noion/jgbemu/cpu/RegisterType.java
@@ -0,0 +1,19 @@
+package ru.noion.jgbemu.cpu;
+
+public enum RegisterType {
+    NONE,
+    A,
+    F,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+    AF,
+    BC,
+    DE,
+    HL,
+    SP,
+    PC;
+}
